7b0c663cbb86260c54356b1deb65400437e04bd2
matthova <matthova87@gmail.com>
2017-05-17 23:17:17 -0700

    Egghead RR5

--- src/App.jsx
+++ src/App.jsx
@@ -2,36 +2,20 @@ import React from 'react';
 import {
   BrowserRouter as Router,
   Route,
-  NavLink,
 } from 'react-router-dom';
 
-import './App.css';
-
-const isActiveFunc = (match, location) => {
-  return match;
-};
-
-const Links = () => (
-  <nav>
-    <NavLink exact activeClassName="active" to="/">Home</NavLink>
-    <NavLink activeStyle={{ color: 'red' }} to="/about">About</NavLink>
-    <NavLink
-      isActive={isActiveFunc}
-      activeClassName="active"
-      to="/contact"
-    >
-      Contact
-    </NavLink>
-  </nav>
-);
-
-const App = () => (
+const App = (props) => (
   <Router>
     <div>
-      <Links />
-      <Route exact path="/" render={() => <h1>Home</h1>} />
-      <Route path="/about" render={() => <h1>About</h1>} />
-      <Route path="/contact" render={() => <h1>Contact</h1>} />
+      <Route
+        path="/:page?/:subpage?"
+        render={({ match }) => (
+          <h1>
+            PAGE: {match.params.page || 'Home'} <br />
+            SUBPAGE: {match.params.subpage || 'No Subpage'}
+          </h1>
+        )}
+      />
     </div>
   </Router>
 );
